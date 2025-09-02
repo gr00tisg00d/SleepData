2bdef87fcce439c519fad450f205290520c5f8ee
sleepinbliss <3rdvisionn@gmail.com>
2025-09-02 13:34:33 -0500

    [1.02] Create user options

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// ask for input
+Console.WriteLine("Enter 1 to create data file.");
+Console.WriteLine("Enter 2 to parse data file.");
+Console.WriteLine("Enter anything else to quit.");
+
+// input response
+string? resp = Console.ReadLine();
+
+if (resp == "1")
+{
+    // TODO: create data file.
+}
+else if (resp == "2")
+{
+    // TODO: parse data file.
+}
