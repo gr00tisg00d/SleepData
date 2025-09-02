02c56a8d3b9a88315865f7953bde1e44354f4c81
sleepinbliss <3rdvisionn@gmail.com>
2025-09-02 13:55:27 -0500

    [1.04] Determine how many weeks

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// ask for input
+using System.Data;
+
 Console.WriteLine("Enter 1 to create data file.");
 Console.WriteLine("Enter 2 to parse data file.");
 Console.WriteLine("Enter anything else to quit.");
@@ -13,6 +15,13 @@ if (resp == "1")
     Console.WriteLine("How many weeks of data is needed?");
     // input the response (convert to int)
     int weeks = Convert.ToInt32(Console.ReadLine());
+    // determine start and end date
+    DateTime today = DateTime.Now;
+    // we want full weeks sunday - saturday
+    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+    // subtract # weeks from endDate to get startDate
+    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+    Console.WriteLine(dataDate);
 }
 else if (resp == "2")
 {
